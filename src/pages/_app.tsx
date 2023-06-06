802a1ddabff0fcb9bc2e1f8364f04eaa7c5c06fb
unknown <randyofgod666@gmail.com>
2023-06-06 18:11:28 -0400

    segundo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '@fontsource/roboto/300.css';
+import '@/styles/globals.css'
+
+import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import { darkTheme, lightTheme } from '@/themes';
+import { UIProvider } from '@/context/ui';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/components/layouts';
+import { Typography } from '@mui/material';
+
+
+export default function Home() {
+  return (
+    <Layout>
+      <Typography variant='h1' color='primary'>OPENJIRA</Typography>
+    </Layout>
+  )
+}
