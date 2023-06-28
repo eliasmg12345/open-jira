5da60de20651fc47e89b6751d895ff951baac51b
eliasmg12345 <randyofgod666@gmail.com>
2023-06-28 16:24:36 -0400

    mongo1

--- src/context/entries/EntriesProvider.tsx
+++ src/context/entries/EntriesProvider.tsx
@@ -1,34 +1,16 @@
-import { FC, useReducer } from 'react'
+import { FC, useReducer, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 import { Entry } from '@/interfaces';
 import { EntriesContext, entriesReducer } from './';
+import { entriesApi } from '@/apis';
 
 export interface EntriesState {
     entries: Entry[];
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-    entries: [
-        {
-            _id: uuidv4(),
-            description: 'entrada: err lorem kloerm dolor sui elit sunt qui dolor la',
-            status: 'pending',
-            createdAt: Date.now()
-        },
-        {
-            _id: uuidv4(),
-            description: 'progreso: fthfgh lo kloerm dolor sui elit sunt qui dolor la',
-            status: 'in-progress',
-            createdAt: Date.now() - 100000,
-        },
-        {
-            _id: uuidv4(),
-            description: 'terminda: fghglorem kloerm dolor sui elit sunt qui dolor la',
-            status: 'finished',
-            createdAt: Date.now() - 1000000,
-        },
-    ],
+    entries: [],
 }
 
 export const EntriesProvider: FC = ({ children }) => {
@@ -47,8 +29,19 @@ export const EntriesProvider: FC = ({ children }) => {
     }
 
     const updateEntry = (entry: Entry) => {
-        dispatch({type: '[Entry] - Entry-Updated', payload:entry});
+        dispatch({ type: '[Entry] - Entry-Updated', payload: entry });
     }
+
+    const refreshEntries = async () => {
+        const { data } = await entriesApi.get<Entry[]>('/entries');
+        dispatch({ type: '[Entry] - Refresh-Data', payload: data })
+
+    }
+
+    useEffect(() => {
+        refreshEntries();
+    }, [])
+
 
     return (
         <EntriesContext.Provider value={{
